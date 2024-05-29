9883190ba4ba3f7c569061b399b67574973a635a
duc-canh <canhnd@toprate.io>
2024-05-29 19:13:48 +0700

    oauth2

--- src/main/java/com/example/demo_di_ioc/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo_di_ioc/security/WebSecurityConfig.java
@@ -18,12 +18,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        // securedEnabled = true,
-        // jsr250Enabled = true,
-        prePostEnabled = true)
+
 public class WebSecurityConfig {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
@@ -58,21 +57,31 @@ public class WebSecurityConfig {
         http.csrf(csrf -> csrf.disable())  // Vô hiệu hóa CSRF
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
+                                .requestMatchers("/oauth2/user").authenticated()
                                 .requestMatchers("/api/user/**").authenticated() // Yêu cầu xác thực cho /api/v1/**
                                 .requestMatchers("/api/private/**").authenticated()
                                 .requestMatchers("/api/auth/logout").authenticated()
+                                .requestMatchers("/oauth2/login-success").authenticated()
                                 .anyRequest().permitAll() // Mở quyền truy cập cho tất cả các yêu cầu khác
                 )
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+//                .sessionManagement(sessionManagement ->
+//                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
                 .headers(headers -> headers
                         .frameOptions(frameOptions -> frameOptions
                                 .sameOrigin()
                         )
                 )
                 .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler); // Thêm bộ lọc JWT;
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(unauthorizedHandler) // Xử lý lỗi xác thực
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/oauth2/login-success")
+                                .failureUrl("/oauth2/login-failure")
+                ); // Thêm bộ lọc JWT;
 
         return http.build();
     }
